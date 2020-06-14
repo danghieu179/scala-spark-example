ea08138bb39ae26c0b9a03086ef40e2741527c39
Nguyễn Đăng Hiếu <danghieu1709@gmail.com>
2020-06-14 17:18:27 +0700

    update path

--- src/main/scala/ReadParquetFile.scala
+++ src/main/scala/ReadParquetFile.scala
@@ -34,7 +34,7 @@ object ReadParquetFile {
     val folderName = fileName.toString.split("\\.")(0)
     // Create new folder for contain report files
     val currentDirectory = new java.io.File(".").getCanonicalPath
-    val folderPath = "/opt/"+folderName
+    val folderPath = "/home/hadoop/"+folderName
     new java.io.File(folderPath).mkdirs
     println(folderPath)
     // run function compute
